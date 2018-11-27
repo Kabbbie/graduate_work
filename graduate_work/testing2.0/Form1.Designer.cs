c7036800c5eba6c875820d60e8bd43f25687c612
Kabbbie <dash.the.programmist@gmail.com>
2018-11-27 19:38:52 +0300

    Initial commit

---
+++ graduate_work/testing2.0/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace testing2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ib1 = new Emgu.CV.UI.ImageBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.ib1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ib1
+            // 
+            this.ib1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ib1.Enabled = false;
+            this.ib1.Location = new System.Drawing.Point(12, 12);
+            this.ib1.Name = "ib1";
+            this.ib1.Size = new System.Drawing.Size(400, 300);
+            this.ib1.TabIndex = 2;
+            this.ib1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(871, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 282);
+            this.textBox1.TabIndex = 3;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(900, 321);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(172, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 1;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(900, 372);
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(172, 45);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Value = 1;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(900, 423);
+            this.trackBar3.Maximum = 300;
+            this.trackBar3.Minimum = -300;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(172, 45);
+            this.trackBar3.TabIndex = 6;
+            this.trackBar3.Value = 1;
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(900, 474);
+            this.trackBar4.Maximum = 300;
+            this.trackBar4.Minimum = -300;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(172, 45);
+            this.trackBar4.TabIndex = 7;
+            this.trackBar4.Value = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 723);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ib1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ib1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox ib1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+    }
+}
+
---
+++ graduate_work/testing2.0/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
+namespace testing2._0
+{
+    public partial class Form1 : Form
+    {
+        #region Переменные
+        
+
+        Capture capweb;
+        const int siteCount = 21;//Количество сайтов(сигналов) 
+        int sqr = (int)Math.Ceiling(Math.Sqrt(siteCount));//Корень для определления квадрата вороного
+        bool bln = false;//Пауза на видео
+        int N;// проверка при чтение с сервера
+        short[] t;
+        short[][] g = new short[siteCount][];
+        int z = 0, r = 0;
+
+        //int minX;
+        //int maxX;
+        //int minY;
+        //int maxY;
+
+        int last_yq=0;
+        int last_xq=0;
+
+
+        #endregion
+
+        #region WEBCAM
+
+        private void GetVideo(object sender, EventArgs e)
+        {
+            Image<Bgr, Byte> Kadr = capweb.QueryFrame().Resize(400, 300, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);  // Получаем кадр с вебкамеры
+            
+            Image<Gray, Byte> gray = Kadr.Convert<Gray, Byte>().PyrDown().PyrUp();   //Convert the image to grayscale and filter out the noise
+
+            Gray cannyThreshold = new Gray(180);
+            Gray cannyThresholdLinking = new Gray(120);
+            Gray circleAccumulatorThreshold = new Gray(120);
+
+            Image<Gray, Byte> cannyEdges = gray.Canny(cannyThreshold, cannyThresholdLinking);
+            LineSegment2D[] lines = cannyEdges.HoughLinesBinary(
+                1, //Distance resolution in pixel-related units
+                Math.PI / 45.0, //Angle resolution measured in radians.
+                20, //threshold
+                30, //min Line width
+                10 //gap between lines
+                )[0]; //Get the lines from the first channel
+           
+            #region Find rectangles          
+            List<MCvBox2D> boxList = new List<MCvBox2D>();
+
+            using (MemStorage storage = new MemStorage()) //allocate storage for contour approximation
+                for (Contour<Point> contours = cannyEdges.FindContours(); contours != null; contours = contours.HNext)
+                {
+                    Contour<Point> currentContour = contours.ApproxPoly(contours.Perimeter * 0.05, storage);
+
+                    if (contours.Area > 500 && contours.Area < 1400) //only consider contours with area greater than 700 and lesser than 1200
+                    {
+                        if (currentContour.Total == 4) //The contour has 4 vertices.
+                        {
+                            #region determine if all the angles in the contour are within the range of [0, 180] degree
+                            bool isRectangle = true;
+                            Point[] pts = currentContour.ToArray();
+                            LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                            for (int i = 0; i < edges.Length; i++)
+                            {
+                                double angle = Math.Abs(
+                                   edges[(i + 1) % edges.Length].GetExteriorAngleDegree(edges[i]));
+                                if (angle < 0 || angle > 180)
+                                {
+                                    isRectangle = false;
+                                    break;
+                                }
+                            }
+                            #endregion
+
+                            if (isRectangle)
+                            {
+                                boxList.Add(currentContour.GetMinAreaRect());
+                            }
+                        }
+                    }
+                }
+            #endregion
+            #region draw triangles and rectangles
+            Image<Bgr, Byte> triangleRectangleImage = Kadr.Copy();
+            foreach (MCvBox2D box in boxList)
+                Kadr.Draw(box, new Bgr(Color.DarkOrange), 3);
+
+            #endregion
+            if (boxList.Count > 0)
+            {
+                
+                int minX = Convert.ToInt32(boxList[0].center.X);
+                int maxX = minX;
+                int minY = Convert.ToInt32(boxList[0].center.Y);
+                int maxY = minY;
+                for (int i = 0; i < boxList.Count; i++)
+                {
+                    int a1 = Convert.ToInt32(boxList[i].center.X), a2 = Convert.ToInt32(boxList[i].center.Y);
+                    if (minX > a1) minX = a1;
+                    else if (maxX < a1) maxX = a1;
+
+                    if (minY > a2) minY = a2;
+                    else if (maxY < a2) maxY = a2;
+                }
+
+
+                int yq = maxY - minY;
+                int xq = maxX - minX;
+                
+                int mX = trackBar1.Value;
+                int mY = trackBar2.Value;
+                int trX = trackBar3.Value;
+                int trY = trackBar4.Value;
+                if (xq > 0 && yq > 0)
+                    DisplayPic(Kadr, minX, maxX, minY, maxY,mX,mY,trX,trY);
+                    
+                last_yq = yq;
+                last_xq = xq;
+                
+            }
+
+            
+            ib1.Image = Kadr;
+        }
+        void DisplayPic(Image<Bgr, Byte> bit, int startX, int endX, int startY, int endY,int multX, int multY,int transX,int transY)
+        {
+
+            #region Resize
+            int lengX = (endX - startX) * multX;
+            int lengY = (endY - startY) * multY;
+            #endregion
+
+            Bitmap bitmap = bit.Bitmap;
+            Size size = new Size(lengX, lengY);
+            Bitmap rentbit = new Bitmap(Image.FromFile("wado1.png"), size);
+            int k=0;
+            int l=0;
+            
+            for(int x = startX;x<startX+lengX;x++)
+            {
+                
+                for (int y = startY; y < startY + lengY; y++)
+                {
+                    Color clrPix = rentbit.GetPixel(k, l);
+                    if(x + transX < bitmap.Width && y + transY < bitmap.Height)
+                        bitmap.SetPixel(x + transX, y + transY, clrPix);
+                    
+                    l++;
+                    
+                }
+                l = 0;
+                k++;
+                
+            }
+            rentbit.Dispose();
+            
+        }
+        void Video()
+        {
+            try
+            {
+                capweb = new Capture();
+
+            }
+            catch (NullReferenceException e)
+            {
+                MessageBox.Show("Подключите камеру");
+            }
+            Application.Idle += GetVideo;//ProcessFrameAndUpdateGUI;
+            bln = true;
+        }
+
+
+        #endregion
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Video();
+        }
+
+        private void Display_Image()
+        {
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
